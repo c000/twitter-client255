01bd53f33c450119a023aa13274b323d893bb6ff
c000 <NetArch>
2013-04-03 12:27:39 +0900

    Implement simple options

--- Main.hs
+++ Main.hs
@@ -1,4 +1,6 @@
 
+import System.Console.GetOpt
+import System.Environment
 import System.Directory
 import System.FilePath
 
@@ -10,6 +12,18 @@ defaultConfig = do
     basePath <- getAppUserDataDirectory "client255"
     return $ Config (basePath </> "cred")
 
+parseArgs :: [String] -> ([Config -> Config], [String], [String])
+parseArgs = getOpt RequireOrder options
+
+options :: [OptDescr (Config -> Config)]
+options =
+    [ Option ['c'] ["credential"] (ReqArg (\path opts -> opts {credPath = path}) "PATH") "Credential path"
+    -- , Option ['u'] ["update"] (ReqArg $ \_ -> id) "Tweet to twitter"
+    ]
+
 main = do
     config <- defaultConfig
-    runClient config
+    args <- getArgs
+    case parseArgs args of
+        (o,[],[]) -> runClient (foldl (flip id) config o)
+        (_,_,err) -> ioError (userError (concat err ++ usageInfo "" options))
